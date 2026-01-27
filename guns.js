f282d7f81d42898af06980e5234b71f95321d586
.Punk <67797794+ArtworkPunk@users.noreply.github.com>
2026-01-27 20:38:30 +0300

    ABI ARMORY V1.0🔥
    ARENA BREAKOUT INFINITE ⚡

---
+++ guns.js
@@ -0,0 +1,35 @@
+// Database of Weapons for Arena Breakout Infinite (Season 4 Full Roster)
+// Total Weapons: 78
+// Tiers available: 'meta-season', 'meta', 'good', 'trash'
+
+    //{ id: 100, folder: "an94", name: "AN-94", price: "~90k", tier: "meta-season", code: "" },
+
+
+
+const weaponsData = [
+    { id: 100, folder: "an94", name: "AN-94", price: "~250k", tier: "meta-season", code: "Модификация оружия-3fv3nO2YZoU8EP4P4" },
+    { id: 101, folder: "amb17", name: "AMB-17", price: "~200k", tier: "meta-season", code: "Модификация оружия-3fv3nO2YZoU8EP4o4" },
+    { id: 102, folder: "t191", name: "T191", price: "~500k", tier: "meta-season", code: "Модификация оружия-3fv3nO2YZoU8EP4q4" },
+    { id: 103, folder: "m4a1", name: "M4A1", price: "~270k", tier: "meta", code: "Модификация оружия-3fv3nO2YZoU8EP4O4" },
+    { id: 104, folder: "akm", name: "AKM", price: "~110k", tier: "trash", code: "Модификация оружия-3fv3nO2YZoU8EP4x4" },
+    { id: 105, folder: "fal", name: "FAL (TAP MODE)", price: "~150k", tier: "good", code: "Модификация оружия-3fv3nO2YZoU8EP4D4" },
+    { id: 106, folder: "h416", name: "H416", price: "~360k", tier: "meta", code: "Модификация оружия-3fv3nO2YZoU8EP4j4" },
+    { id: 107, folder: "mdr", name: "MDR (BURST MODE)", price: "~205k", tier: "meta", code: "Модификация оружия-3fv3nO2YZoU8EP424" },
+    { id: 108, folder: "ak102", name: "AK-102", price: "~130k", tier: "meta-season", code: "Модификация оружия-3fv3nO2YZoU8EP424" },
+    { id: 109, folder: "ak74h", name: "AK-74H", price: "~110k", tier: "good", code: "Модификация оружия-3fv3nO2YZoU8EP4X4" },
+    { id: 110, folder: "aks74y", name: "AKC-74Y", price: "~100k", tier: "trash", code: "Модификация оружия-3fv3nO2YZoU8EP4i4" },
+    { id: 111, folder: "aug", name: "AUG", price: "~225k", tier: "meta", code: "Модификация оружия-3fv3nO2YZoU8EP4w4" },
+    { id: 112, folder: "f2000", name: "F2000", price: "~215k", tier: "meta", code: "Модификация оружия-3fv3nO2YZoU8EP4E4" },
+    { id: 113, folder: "ar57", name: "AR-57", price: "~250k", tier: "meta", code: "Модификация оружия-3fv3nO2YZoU8EP454" },
+    { id: 114, folder: "ace31", name: "ACE31", price: "~145k", tier: "trash", code: "Модификация оружия-3fv3nO2YZoU8EP4V4" },
+    { id: 115, folder: "aek", name: "AEK", price: "~250k", tier: "good", code: "Модификация оружия-3fv3nO2YZoU8EP4c4" },
+    { id: 116, folder: "scarl", name: "SCAR-L", price: "~160k", tier: "good", code: "Модификация оружия-3fv3nO2YZoU8EP4e4" },
+    { id: 117, folder: "t951", name: "T951", price: "~140k", tier: "meta", code: "Модификация оружия-3fv3nO2YZoU8EP4G4" },
+    { id: 118, folder: "toz", name: "TOZ", price: "~145k", tier: "good", code: "Модификация оружия-3fv3nO2YZoU8EP4r4" },
+    { id: 119, folder: "sg550", name: "SG550", price: "~145k", tier: "good", code: "Модификация оружия-3fv3nO2YZoU8EP4r4" },
+    { id: 120, folder: "g3", name: "G3", price: "~115k", tier: "good", code: "Модификация оружия-3fv3nO2YZoU8EP4W4" },
+    { id: 121, folder: "ak12", name: "AK12", price: "~240k", tier: "meta-season", code: "Модификация оружия-3fv3nO2YZoU8EP4u4" },
+    { id: 122, folder: "mcx", name: "MCX", price: "~175k", tier: "meta-season", code: "Модификация оружия-3fv3nO2YZoU8EP4d4" },
+    { id: 123, folder: "pcc9", name: "PCC-9", price: "~117k", tier: "meta-season", code: "Модификация оружия-3fv3nO2YZoU8EP434" },
+    { id: 124, folder: "zc807", name: "ZC807", price: "~172k", tier: "meta", code: "Модификация оружия-3fv3nO2YZoU8EP4S4" },
+];
